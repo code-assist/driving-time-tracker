556f3a7635787992c1c60f3eadb54fa72161a047
Ethan Hall <ethan@ehdev.io>
2013-04-02 00:18:36 -0500

    Adding copyright

---
+++ src/main/java/io/ehdev/android/drivingtime/adapter/DrivingRecordAdapter.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2013 by Ethan Hall
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package io.ehdev.android.drivingtime.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import io.ehdev.android.drivingtime.adapter.pojo.DrivingRecord;
+import io.ehdev.android.drivingtime.view.DisplayRecordRow;
+
+import java.util.List;
+
+public class DrivingRecordAdapter extends BaseAdapter {
+
+    private Context viewContext;
+    private List<DrivingRecord> recordList;
+
+    public DrivingRecordAdapter(Context viewContext, List<DrivingRecord> recordList){
+        this.viewContext = viewContext;
+        this.recordList = recordList;
+    }
+    @Override
+    public int getCount() {
+        return recordList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return recordList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(null == view || !(view instanceof DisplayRecordRow))
+            view = new DisplayRecordRow(viewContext);
+
+        return view;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
